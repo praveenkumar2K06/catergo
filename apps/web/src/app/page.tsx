5dcea9f8baf8ee199c0fc5636a3bf0136d7a4004
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2025-08-27 12:19:42 +0530

    🔥 Remove Next JS

--- apps/web/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OnboardingFlow } from "@/components/onboarding/onboarding-flow";
-
-export default function Home() {
-	return (
-		<main className="min-h-screen bg-gray-50">
-			<OnboardingFlow />
-		</main>
-	);
-}
