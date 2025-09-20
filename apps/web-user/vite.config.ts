b2ad918458774cb516ada26a09f2c466c8dfade1
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2025-09-20 14:09:44 +0530

    ➕ Vite Tanstack Devtools to User Side

--- apps/web-user/vite.config.ts
+++ apps/web-user/vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -7,6 +8,7 @@ import viteTsConfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
+		devtools(),
 		tanstackRouter(),
 		react(),
 		viteTsConfigPaths({
